a818d8629bb41221f920112a69f0640d52a1a0d5
Prateekcodi <prateekpal515@gmail.com>
2024-09-16 18:41:05 +0530

    challenge 1 and 2 completed

--- test.js
+++ test.js
@@ -1,5 +1,60 @@
+/*typeof
 let js=null;
 if(js==null){
   console.log(typeof js);
 }
+*/
 
+/*
+// coding first chanllenge 1
+// data1
+const massMark=78;
+const heightMark=1.69;
+const massJohn=92;
+const heightJohn=1.95;
+// data 2
+// const massMark=95;
+// const heightMark=1.88;
+// const massJohn=85;
+// const heightJohn=1.75;
+
+const markBmi=massMark/(heightMark**2);
+const johnBmi=massJohn/(heightJohn**2);
+// console.log(markBmi,johnBmi);
+
+let markBmihigher=false;
+if(markBmi>johnBmi){
+  markBmihigher=true;
+}
+if(markBmihigher){
+  //coding challenege 2
+  console.log(`Mark's BMI ${markBmi} is higher than John's ${johnBmi}`);
+}else{
+  console.log(`John's BMI ${markBmi} is higher than Mark's! ${johnBmi}`);
+  
+}
+*/
+/*
+// template litrals
+const names="prateek";
+const age=45;
+const birthyear =2004;
+const currentyear=2024
+const job="student"
+const litrals=`i'm ${names} and my age is ${currentyear-birthyear} year old and i'm ${job}!`;
+console.log(litrals);
+console.log(`hello world! \ni'm ${job} here`);
+console.log(`string    multiple
+lines`);
+*/
+/*
+//conversion
+let score=-32;
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+// console.log(1+3+'3');
+// falsy values there are 5 and else are truthy
+// console.log(Boolean(null))
+console.log(Boolean("crore"))
